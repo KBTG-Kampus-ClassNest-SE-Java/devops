7a7926ada844433bd89371fb02ab19b42e5ce689
bhuridech <aorjor.bootcamp@gmail.com>
2024-03-18 22:06:14 +0700

    add petstore

---
+++ devops-java-example/petstore/src/main/java/com/example/petstore/pet/controller/PetController.java
@@ -0,0 +1,30 @@
+package com.example.petstore.pet.controller;
+
+import com.example.petstore.pet.model.Pet;
+import com.example.petstore.pet.service.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pets")
+public class PetController {
+    @Autowired
+    private PetService petService;
+
+    @GetMapping("/hi")
+    public String sayHi() {
+        return "hi, from Continuous Deployment!";
+    }
+
+    @GetMapping
+    public List<Pet> allPet() {
+        return petService.allPets();
+    }
+
+    @PostMapping
+    public Pet addPet(@RequestBody Pet pet) {
+        return petService.addPet(pet);
+    }
+}
---
+++ devops-java-example/petstore/src/test/java/com/example/petstore/PetStoreApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.petstore;
+
+
+import org.junit.jupiter.api.Test;
+
+class PetStoreApplicationTests {
+	@Test
+	void contextLoads() {
+		assert true;
+	}
+}
